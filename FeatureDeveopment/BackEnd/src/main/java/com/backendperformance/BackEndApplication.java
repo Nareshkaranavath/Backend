f349c36fcad069db5b71387b046aa7258fbba283
Nareshkaranavath <140869848+Nareshkaranavath@users.noreply.github.com>
2024-08-06 13:23:13 +0530

    Add files via upload
    backend service add

---
+++ FeatureDeveopment/BackEnd/src/main/java/com/backendperformance/BackEndApplication.java
@@ -0,0 +1,10 @@
+package com.backendperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+}
